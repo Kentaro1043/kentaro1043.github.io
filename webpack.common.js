c30deea335a46f05bd1656e164aab555d800566f
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2025-01-09 13:41:29 +0900

    :art: 全ファイルフォーマット

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,12 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: {
-    app: './js/app.js',
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-    filename: './js/app.js',
-  },
+	entry: {
+		app: "./js/app.js"
+	},
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		clean: true,
+		filename: "./js/app.js"
+	}
 };
