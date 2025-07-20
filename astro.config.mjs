fd2704ad8947a85a6fb7a3f8ce091df0d680b8c1
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2025-07-21 02:03:07 +0900

    chore: 🔧 Astro設定の回避策適用

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
@@ -13,6 +13,13 @@ import icon from "astro-icon";
 export default defineConfig({
   site: "https://www.kentaro1043.com",
   integrations: [mdx(), sitemap(), react(), icon()],
+
+  // https://docs.astro.build/en/guides/images/#default-image-service
+  // 何故かsharpが使えなかったので回避策
+  // TODO: sharpを直せたら削除
+  image: {
+    service: passthroughImageService(),
+  },
 
   vite: {
     plugins: [tailwindcss()],
